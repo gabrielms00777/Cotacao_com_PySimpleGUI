53286e4761e01e032ea62db0961f11ae1526e90b
Gabriel Moreira <83798870+gabrielms00777@users.noreply.github.com>
2022-01-31 19:35:55 -0300

    Update Cotacao_com_pysimplegui.py

--- Cotacao_com_pysimplegui.py
+++ Cotacao_com_pysimplegui.py
@@ -1,25 +1,43 @@
 import PySimpleGUI as sg
-from PySimpleGUI.PySimpleGUI import Window
 import requests
 
-sg.theme('Dark')
+class Cotacao:
+    def __init__(self):
+        sg.theme('SystemDefault')
+        layout = [
+            [sg.Text('Bem vindo ao app de cotação de moedas')],
+            [sg.Text('Essas são as moedas mais cotadas')],
+            [sg.Text('USD:'), sg.Text(self.pegar_cotacao('usd'))],
+            [sg.Text('EUR:'), sg.Text(self.pegar_cotacao('eur'))],
+            [sg.Text('BTC:'), sg.Text(self.pegar_cotacao('btc'))],
+            [sg.Text('ETH:'), sg.Text(self.pegar_cotacao('eth'))],
+            [sg.Text('Qual moeda vai quer cotar agora?')],
+            [sg.Input(key='moeda')],
+            [sg.Button('Cotar')],
+            [sg.Output(size=(50, 7))]
+        ]
+        self.janela = sg.Window('Cotações', layout)
 
-def Pegar_cotacao(moeda):
-    link = f'https://economia.awesomeapi.com.br/last/{moeda}-BRL'
-    requisicao = requests.get(link)
-    dic_requisicao = requisicao.json()
-    cotacao = dic_requisicao[f'{moeda}BRL']['bid']
-    return cotacao
+    def pegar_cotacao(self,moeda):
+        moeda = moeda.upper()
+        link = f'https://economia.awesomeapi.com.br/last/{moeda}-BRL'
+        requisicao = float(requests.get(link).json()[f'{moeda}BRL']['bid'])
+        requisicao = "${:,.2f}".format(requisicao)
+        print(f' Cotacao da moeda {moeda}: {requisicao}')
+        return requisicao
 
-layout = [
-    [sg.Text(f'Dolar: {Pegar_cotacao("USD")}',key='dolar')],
-    [sg.Text(f'Euro: {Pegar_cotacao("EUR")}',key='euro')],
-    [sg.Text(f'Bitcoin: {Pegar_cotacao("BTC")}',key='btc')],
-    [sg.Text(f'Etherum: {Pegar_cotacao("ETH")}',key='eth')]
-]
+    def iniciar(self):
+        while True:
+            event, valores = self.janela.read()
+            if event == sg.WINDOW_CLOSED:
+                break
+            elif event == 'Cotar':
+                self.pegar_cotacao(valores['moeda'])
 
-window = sg.Window('Cotação Medas', layout)
 
-window.read()
+cot = Cotacao()
+cot.iniciar()
+
+
 
 
